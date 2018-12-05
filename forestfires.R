9289f2d82a3fd49e9870f7171243142268c489b9
pauvela <pau.vela.garcia@gmail.com>
2018-12-05 17:42:11 +0100

    Forest Fires code

---
+++ forestfires.R
@@ -0,0 +1,88 @@
+####Install Packages####
+install.packages("pacman")
+pacman::p_load(dplyr,caret,ggplot2,reshape2,corrplot,party,rpart,rpart.plot) 
+####Install Database####
+library(readr)
+forestfires <- read_csv("ubiqum/BET365DB/forestfires.csv")
+View(forestfires)
+FFires<-forestfires
+####Pre-Process####
+str(forestfires)
+summary(forestfires)
+hist(forestfires$area)
+plot(forestfires$area)
+forestfires$month <-NULL
+forestfires$day <-NULL
+##each column plotted and log for area,RH,wind and Y ##
+forestfires$log.area<- log(forestfires$area +1)
+hist(forestfires$log.area)
+forestfires$log.RH<- log(forestfires$RH)
+forestfires$log.wind<- log(forestfires$wind)
+forestfires$log.Y<- log(forestfires$Y)
+forestfires$log.DC<- log(forestfires$DC)
+##decision tree and plots to know which variables explain log.area##
+decisionTreeFFires <-rpart(log.area~.-area, data= forestfires)
+decisionTreeFFires
+ff <- melt(forestfires)
+ggplot(ff,aes(x = value)) + 
+  facet_wrap(~variable, scales = "free_x") + 
+  geom_histogram()
+##Correlations##
+correlationFF<-cor(forestfires)
+correlationFF
+View(correlationFF)
+corrplot(correlationFF, method="circle", type="lower", order="hclust")
+write.csv(correlationFF, file= "CorrelationFF.csv")
+
+####Data Partition####
+
+TrainFF<- createDataPartition(forestfires$log.area, p=0.80, list= FALSE)
+trainForest<- forestfires[TrainFF,]
+testForest<- forestfires[-TrainFF,]
+
+####Prediction####
+fitControl <- trainControl(method = "cv",number = 10)
+##lm##
+LinearModel <- lm(log.area ~temp+DMC+X + RH ,
+         data = forestfires)
+LinearModel
+summary(LinearModel)
+predictionLM <- predict(LinearModel, select(testForest,FFMC,temp ,DMC ,log.DC,log.wind,X,log.Y,RH))
+predictionNoLog<- exp(predictionLM)-1
+predictionLMNoLog<- data.frame(pred= predictionNoLog, obs=testForest$area)
+View(predictionNoLog)
+
+ggplot(predictionLMNoLog, aes(x = pred, y = obs)) + geom_point(alpha = 0.5,
+position = position_jitter(width=0.2)) + geom_abline(colour = "blue") + ggtitle("LM Prediction")
+
+
+##SVM##
+FFSVM<-train(log.area ~ 0+temp+DMC+X+RH,
+                  data=trainForest,
+                  method='svmRadial',
+                  trControl = fitControl,
+                  preProcess = c("zv", "center", "scale"))
+
+print(FFSVM)
+
+predictionFFSVM<-predict(FFSVM,testForest)
+predictionFFNoLog<- exp(predictionFFSVM)-1
+predictionFFSVMNoLog<- data.frame(pred= predictionFFNoLog, obs=testForest$area)
+View(predictionFFSVMNoLog)
+testForest$predictionFFSVMNoLog<-predictionFFSVM
+
+ggplot(predictionFFSVMNoLog, aes(x = pred, y = obs)) + geom_point(alpha = 0.5,
+                                                               position = position_jitter(width=0.2)) + geom_abline(colour = "blue") + ggtitle("SVM Prediction")
+##RandomForest##
+RFFF<-train(log.area~ 0+temp+DMC+X+RH, data= trainForest, method="rf",
+              trControl=fitControl, ntree=50, do.trace=10)
+
+varImp(RFFF)
+
+predictionRFFF<-predict(RFFF,testForest)
+View(predictionRFFF)
+predictionRFNoLog<- exp(predictionRFFF)-1
+predictionFFRFNoLog<- data.frame(pred= predictionRFNoLog, obs=testForest$area)
+View(predictionFFRFNoLog)
+testForest$predictionFFRFNoLog<-predictionFFRF
+postResample(pred = predictionRFFF, obs = testForest$log.area)
